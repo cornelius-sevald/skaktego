66cdc7f3a33dba5986186cc151c6a69a1ff78ea8
Cornelius Sevald-Krause <cornelius.sevald@gmail.com>
2020-01-11 01:18:01 +0100

    Add ability to load PNG images
    Also convert BMP images to PNG & clean up code.

--- src/Program.cs
+++ src/Program.cs
@@ -7,34 +7,54 @@ namespace skaktego {
     class Program {
         const int SCREEN_WIDTH = 640;
         const int SCREEN_HEIGHT = 480;
+        // Size of background tiles.
+        const int TILE_SIZE = 160;
         const string RESOURCE_PATH = "resources/";
 
         static TextWriter stdout = Console.Out;
 
         static int Main(string[] args) {
+            // Initialize SDL
             if (SDL.SDL_Init(SDL.SDL_INIT_EVERYTHING) != 0) {
                 LogSDLError(stdout, "SDL_Init");
                 return 1;
             }
 
-            IntPtr window = SDL.SDL_CreateWindow("skaktego", 100, 100, SCREEN_WIDTH,
-                SCREEN_HEIGHT, SDL.SDL_WindowFlags.SDL_WINDOW_SHOWN);
-            if (window == IntPtr.Zero) {
-                LogSDLError(stdout, "CreateWindow");
-                SDL.SDL_Quit();
-                return 1;
-            }
-            IntPtr renderer = SDL.SDL_CreateRenderer(window, -1,
-                SDL.SDL_RendererFlags.SDL_RENDERER_ACCELERATED | SDL.SDL_RendererFlags.SDL_RENDERER_PRESENTVSYNC);
-            if (renderer == IntPtr.Zero) {
-                LogSDLError(stdout, "CreateRenderer");
-                SDL.SDL_DestroyWindow(window);
+            // Initialize SDL PNG image loading
+            if ((SDL_image.IMG_Init(SDL_image.IMG_InitFlags.IMG_INIT_PNG) &
+                               (int)SDL_image.IMG_InitFlags.IMG_INIT_PNG) !=
+                               (int)SDL_image.IMG_InitFlags.IMG_INIT_PNG) {
+                LogSDLError(stdout, "IMG_Init");
                 SDL.SDL_Quit();
                 return 1;
             }
 
-            IntPtr background = LoadTexture("background.bmp", renderer);
-            IntPtr image = LoadTexture("image.bmp", renderer);
+            // Create window
+            IntPtr window = SDL.SDL_CreateWindow("skaktego", 100, 100,
+                    SCREEN_WIDTH, SCREEN_HEIGHT,
+                    SDL.SDL_WindowFlags.SDL_WINDOW_SHOWN);
+            if (window == IntPtr.Zero) {
+                LogSDLError(stdout, "CreateWindow");
+                SDL_image.IMG_Quit();
+                SDL.SDL_Quit();
+                return 1;
+            }
+
+            // Create renderer
+            IntPtr renderer = SDL.SDL_CreateRenderer(window, -1,
+                    SDL.SDL_RendererFlags.SDL_RENDERER_ACCELERATED |
+                    SDL.SDL_RendererFlags.SDL_RENDERER_PRESENTVSYNC);
+            if (renderer == IntPtr.Zero) {
+                LogSDLError(stdout, "CreateRenderer");
+                SDL.SDL_DestroyWindow(window);
+                SDL_image.IMG_Quit();
+                SDL.SDL_Quit();
+                return 1;
+            }
+
+            // Load two images
+            IntPtr background = LoadTexture("background.png", renderer);
+            IntPtr image = LoadTexture("image.png", renderer);
             if (background == IntPtr.Zero || image == IntPtr.Zero) {
                 if (background != IntPtr.Zero) {
                     SDL.SDL_FreeSurface(background);
@@ -48,42 +68,48 @@ namespace skaktego {
                 if (window != IntPtr.Zero) {
                     SDL.SDL_DestroyWindow(window);
                 }
+                SDL_image.IMG_Quit();
                 SDL.SDL_Quit();
                 return 1;
             }
 
-            // Put the image coordinates at the center of the screen.
-            int iW, iH;
-            SDL.SDL_QueryTexture(image, out _, out _, out iW, out iH);
+            // The image offset
+            int xOffset = 0, yOffset = 0;
 
+            // The main loop
             SDL.SDL_Event e;
             bool quit = false;
             while (!quit) {
+                // Poll events
                 while (SDL.SDL_PollEvent(out e) != 0) {
                     switch (e.type) {
+                        // Check for quit event
                         case SDL.SDL_EventType.SDL_QUIT:
                             quit = true;
                             break;
                         case SDL.SDL_EventType.SDL_KEYDOWN:
                             switch (e.key.keysym.sym) {
+                                // Check if user pressed 'q'
                                 case SDL.SDL_Keycode.SDLK_q:
                                     quit = true;
                                     break;
+                                // Check wasd & arrow keys
+                                // and offset image accordingly
                                 case SDL.SDL_Keycode.SDLK_UP:
                                 case SDL.SDL_Keycode.SDLK_w:
-                                    iH += 10;
+                                    yOffset -= 10;
                                     break;
                                 case SDL.SDL_Keycode.SDLK_LEFT:
                                 case SDL.SDL_Keycode.SDLK_a:
-                                    iW += 10;
+                                    xOffset -= 10;
                                     break;
                                 case SDL.SDL_Keycode.SDLK_DOWN:
                                 case SDL.SDL_Keycode.SDLK_s:
-                                    iH -= 10;
+                                    yOffset += 10;
                                     break;
                                 case SDL.SDL_Keycode.SDLK_RIGHT:
                                 case SDL.SDL_Keycode.SDLK_d:
-                                    iW -= 10;
+                                    xOffset += 10;
                                     break;
                             }
                             break;
@@ -93,35 +119,43 @@ namespace skaktego {
 
                 SDL.SDL_RenderClear(renderer);
 
-                // Render 4 copies of the background.
-                int bW, bH;
-                SDL.SDL_QueryTexture(background, out _, out _, out bW, out bH);
-                renderTexture(background, renderer, 0, 0);
-                renderTexture(background, renderer, bW, 0);
-                renderTexture(background, renderer, 0, bH);
-                renderTexture(background, renderer, bW, bH);
+                // Determine how many tiles we'll need to fill the screen
+                int xTiles = SCREEN_WIDTH / TILE_SIZE;
+                int yTiles = SCREEN_HEIGHT / TILE_SIZE;
 
-                int x = SCREEN_WIDTH / 2 - iW / 2;
-                int y = SCREEN_HEIGHT / 2 - iH / 2;
-                renderTexture(image, renderer, x, y);
+                // Draw the tiles by calculating their positions
+                for (int i = 0; i < xTiles * yTiles; ++i){
+                    int xT = i % xTiles;
+                    int yT = i / xTiles;
+                    RenderTexture(background, renderer,
+                            xT * TILE_SIZE, yT * TILE_SIZE, TILE_SIZE,
+                            TILE_SIZE);
+                }
+
+                // Draw the foreground image
+                int iW, iH;
+                SDL.SDL_QueryTexture(image, out _, out _, out iW, out iH);
+                int x = SCREEN_WIDTH / 2 - iW / 2 + xOffset;
+                int y = SCREEN_HEIGHT / 2 - iH / 2 + yOffset;
+                RenderTexture(image, renderer, x, y);
 
                 SDL.SDL_RenderPresent(renderer);
             }
 
-
+            // Clean up
             SDL.SDL_FreeSurface(background);
             SDL.SDL_DestroyTexture(image);
             SDL.SDL_DestroyRenderer(renderer);
             SDL.SDL_DestroyWindow(window);
+            SDL_image.IMG_Quit();
             SDL.SDL_Quit();
             return 0;
         }
 
-        /**
-         * Log an SDL error with some error message to the output stream of our choice
-         * @param os The output stream to write the message to
-         * @param msg The error message to write, format will be msg error: SDL_GetError()
-         */
+        /// <summary>
+        /// Log an SDL error with some error message to the output stream of our choice
+        /// <param name="writer">The TextWriter to write to</param>
+        /// <param name="msg">The message to write</param>
         static void LogSDLError(TextWriter writer, string msg) {
             using (writer) {
                 writer.WriteLine(msg + " error: " + SDL.SDL_GetError());
@@ -129,30 +163,40 @@ namespace skaktego {
         }
 
         /// <summary>
-        /// Load a BMP image into a texture on the rendering device
+        /// Load an image into a texture on the rendering device
         /// </summary>
-        /// <param name="file">The BMP image file to load</param>
+        /// <param name="file">The image file to load</param>
         /// <param name="ren">The renderer to load the texture onto</param>
         /// <returns>The loaded texture, or <c>IntPtr.Zero</c> if something went wrong</returns>
         static IntPtr LoadTexture(string file, IntPtr ren) {
             // Construct the full path
             string path = Path.Combine(RESOURCE_PATH, file);
-            //Initialize to null to avoid dangling pointer issues
-            IntPtr texture = IntPtr.Zero;
-            //Load the image
-            IntPtr loadedImage = SDL.SDL_LoadBMP(path);
-            //If the loading went ok, convert to texture and return the texture
-            if (loadedImage != IntPtr.Zero) {
-                texture = SDL.SDL_CreateTextureFromSurface(ren, loadedImage);
-                SDL.SDL_FreeSurface(loadedImage);
-                //Make sure converting went ok too
-                if (texture == IntPtr.Zero) {
-                    LogSDLError(stdout, "CreateTextureFromSurface");
-                }
-            } else {
-                LogSDLError(stdout, "LoadBMP");
+
+            IntPtr texture = SDL_image.IMG_LoadTexture(ren, path);
+            if (texture == IntPtr.Zero) {
+                LogSDLError(stdout, "LoadTexture");
             }
             return texture;
+        }
+
+        /// <summary>
+        /// Draw an SDL_Texture to an SDL_Renderer at position x, y, with some desired
+        /// width and height
+        /// </summary>
+        /// <param name="tex">The source texture we want to draw</param>
+        /// <param name="ren">The renderer we want to draw to</param>
+        /// <param name="x">The x coordinate to draw to</param>
+        /// <param name="y">The y coordinate to draw to</param>
+        /// <param name="w">The width of the texture to draw</param>
+        /// <param name="h">The height of the texture to draw</param>
+        static void RenderTexture(IntPtr tex, IntPtr ren, int x, int y, int w, int h) {
+            //Setup the destination rectangle to be at the position we want
+            SDL.SDL_Rect dst;
+            dst.x = x;
+            dst.y = y;
+            dst.w = w;
+            dst.h = h;
+            SDL.SDL_RenderCopy(ren, tex, IntPtr.Zero, ref dst);
         }
 
         /// <summary>
@@ -163,14 +207,10 @@ namespace skaktego {
         /// <param name="ren">The renderer we want to draw to</param>
         /// <param name="x">The x coordinate to draw to</param>
         /// <param name="y">The y coordinate to draw to</param>
-        static void renderTexture(IntPtr tex, IntPtr ren, int x, int y) {
-            //Setup the destination rectangle to be at the position we want
-            SDL.SDL_Rect dst;
-            dst.x = x;
-            dst.y = y;
-            //Query the texture to get its width and height to use
-            SDL.SDL_QueryTexture(tex, out _, out _, out dst.w, out dst.h);
-            SDL.SDL_RenderCopy(ren, tex, IntPtr.Zero, ref dst);
+        static void RenderTexture(IntPtr tex, IntPtr ren, int x, int y) {
+            int w, h;
+            SDL.SDL_QueryTexture(tex, out _, out _, out w, out h);
+            RenderTexture(tex, ren, x, y, w, h);
         }
     }
 }
