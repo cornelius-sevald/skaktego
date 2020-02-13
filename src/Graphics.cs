678c05105d2dfd0e4c09814cb3e3c386f85ab80b
Cornelius Sevald-Krause <cornelius.sevald@gmail.com>
2020-02-13 02:10:38 +0100

    Wrap common SDL functionality in classes

---
+++ src/Graphics.cs
@@ -0,0 +1,185 @@
+using System;
+using System.IO;
+using SDL2;
+
+namespace skaktego {
+
+    public static class Graphics {
+        public const string RESOURCE_PATH = "resources/";
+
+        public static void InitGraphics() {
+            // Initialize SDL
+            if (SDL.SDL_Init(SDL.SDL_INIT_EVERYTHING) != 0) {
+                throw new SDLException("SDL_Init");
+            }
+
+            // Initialize SDL PNG image loading
+            if ((SDL_image.IMG_Init(SDL_image.IMG_InitFlags.IMG_INIT_PNG) &
+                               (int)SDL_image.IMG_InitFlags.IMG_INIT_PNG) !=
+                               (int)SDL_image.IMG_InitFlags.IMG_INIT_PNG) {
+                throw new SDLException("IMG_Init");
+            }
+        }
+
+    }
+
+    public class Window {
+        public IntPtr WinPtr { get; private set; }
+
+        public Window(string title, int xPos, int yPos, int width, int height) {
+            // Create window
+            IntPtr window = SDL.SDL_CreateWindow(title, xPos, yPos, width, height,
+                    SDL.SDL_WindowFlags.SDL_WINDOW_SHOWN);
+            if (window == IntPtr.Zero) {
+                throw new SDLException("CreateWindow");
+            }
+
+            WinPtr = window;
+        }
+
+        ~Window() {
+            SDL.SDL_DestroyWindow(WinPtr);
+        }
+    }
+
+    public class Renderer {
+        public IntPtr RenPtr { get; private set; }
+
+        public Renderer(Window window) {
+            // Create renderer
+            IntPtr renderer = SDL.SDL_CreateRenderer(window.WinPtr, -1,
+                    SDL.SDL_RendererFlags.SDL_RENDERER_ACCELERATED |
+                    SDL.SDL_RendererFlags.SDL_RENDERER_PRESENTVSYNC);
+            if (renderer == IntPtr.Zero) {
+                throw new SDLException("CreateRenderer");
+            }
+
+            RenPtr = renderer;
+        }
+
+        public void Clear() {
+            SDL.SDL_RenderClear(RenPtr);
+        }
+
+        public void Present() {
+            SDL.SDL_RenderPresent(RenPtr);
+        }
+
+        /// <summary>
+        /// Draw a Texture to this Renderer at some destination Rect
+        /// taking a clip of the texture if desired
+        /// </summary>
+        /// <param name="texture">The source texture we want to draw</param>
+        /// <param name="dst">The destination rectangle to render the texture to</param>
+        /// <param name="clip">The sub-section of the texture to draw (clipping rect)
+        /// default of null draws the entire texture</param>
+        public void RenderTexture(Texture texture, Rect dst, Rect clip) {
+            if (dst != null) {
+                SDL.SDL_Rect _dst = dst.Rct;
+                if (clip != null) {
+                    SDL.SDL_Rect _clip = clip.Rct;
+                    SDL.SDL_RenderCopy(RenPtr, texture.TexPtr, ref _clip, ref _dst);
+                } else {
+                    SDL.SDL_RenderCopy(RenPtr, texture.TexPtr, IntPtr.Zero, ref _dst);
+                }
+            } else {
+                SDL.SDL_RenderCopy(RenPtr, texture.TexPtr, IntPtr.Zero, IntPtr.Zero);
+            }
+        }
+
+        /// <summary>
+        /// Draw a Texture to this Renderer at position x, y, with some desired
+        /// width and height
+        /// </summary>
+        /// <param name="tex">The source texture we want to draw</param>
+        /// <param name="x">The x coordinate to draw to</param>
+        /// <param name="y">The y coordinate to draw to</param>
+        /// <param name="w">The width of the texture to draw</param>
+        /// <param name="h">The height of the texture to draw</param>
+        public void RenderTexture(Texture texture, int x, int y, int w, int h) {
+            //Setup the destination rectangle to be at the position we want
+            Rect dst = new Rect(x, y, h, w);
+            RenderTexture(texture, dst, null);
+        }
+
+        /// <summary>
+        /// Draw a Texture to this Renderer at position x, y, preserving
+        /// the texture's width and height
+        /// </summary>
+        /// <param name="texture">The source texture we want to draw</param>
+        /// <param name="x">The x coordinate to draw to</param>
+        /// <param name="y">The y coordinate to draw to</param>
+        public void RenderTexture(Texture texture, int x, int y) {
+            int w, h;
+            texture.Query(out w, out h);
+            RenderTexture(texture, x, y, w, h);
+        }
+
+        ~Renderer() {
+            SDL.SDL_DestroyRenderer(RenPtr);
+        }
+    }
+
+    public class Texture {
+        public IntPtr TexPtr { get; private set; }
+
+        public Texture(Renderer renderer, string name) {
+            // Construct the full path
+            string path = Path.Combine(Graphics.RESOURCE_PATH, name);
+
+            IntPtr texture = SDL_image.IMG_LoadTexture(renderer.RenPtr, path);
+            if (texture == IntPtr.Zero) {
+                throw new SDLException("LoadTexture");
+            }
+
+            TexPtr = texture;
+        }
+
+        public void Query(out int w, out int h) {
+            SDL.SDL_QueryTexture(TexPtr, out _, out _, out w, out h);
+        }
+
+        ~Texture() {
+            SDL.SDL_DestroyTexture(TexPtr);
+        }
+    }
+
+    public class Rect {
+        public SDL.SDL_Rect Rct { get; private set; }
+        public int XPos {
+            get { return Rct.x; }
+            set { Rct = new SDL.SDL_Rect { x = value, y = YPos, w = Width, h = Height }; }
+        }
+
+        public int YPos {
+            get { return Rct.y; }
+            set { Rct = new SDL.SDL_Rect { x = XPos, y = value, w = Width, h = Height }; }
+        }
+
+        public int Width {
+            get { return Rct.w; }
+            set { Rct = new SDL.SDL_Rect { x = XPos, y = YPos, w = value, h = Height }; }
+        }
+
+        public int Height {
+            get { return Rct.h; }
+            set { Rct = new SDL.SDL_Rect { x = XPos, y = YPos, w = Width, h = value }; }
+        }
+
+        public Rect(int xPos, int yPos, int width, int height) {
+            XPos   = xPos;
+            YPos   = yPos;
+            Width  = width;
+            Height = height;
+        }
+    }
+    
+    public class SDLException : Exception
+    {
+        public SDLException(string message)
+           : base(message + "error: " + SDL.SDL_GetError())
+        {
+        }
+    }
+
+}
--- src/Program.cs
+++ src/Program.cs
@@ -5,86 +5,28 @@ using SDL2;
 
 namespace skaktego {
     class Program {
-        // Screen size
-        const int SCREEN_WIDTH = 640;
-        const int SCREEN_HEIGHT = 480;
+        private const int SCREEN_WIDTH = 640;
+        private const int SCREEN_HEIGHT = 480;
+
         // Size of background tiles
         const int TILE_SIZE = 160;
         // Path of game resources
-        const string RESOURCE_PATH = "resources/";
-
-        static TextWriter stdout = Console.Out;
 
         static int Main(string[] args) {
             DateTime start = DateTime.Now;
 
-            // Initialize SDL
-            if (SDL.SDL_Init(SDL.SDL_INIT_EVERYTHING) != 0) {
-                LogSDLError(stdout, "SDL_Init");
-                return 1;
-            }
+            Graphics.InitGraphics();
 
-            // Initialize SDL PNG image loading
-            if ((SDL_image.IMG_Init(SDL_image.IMG_InitFlags.IMG_INIT_PNG) &
-                               (int)SDL_image.IMG_InitFlags.IMG_INIT_PNG) !=
-                               (int)SDL_image.IMG_InitFlags.IMG_INIT_PNG) {
-                LogSDLError(stdout, "IMG_Init");
-                SDL.SDL_Quit();
-                return 1;
-            }
-
-            // Create window
-            IntPtr window = SDL.SDL_CreateWindow("skaktego", 100, 100,
-                    SCREEN_WIDTH, SCREEN_HEIGHT,
-                    SDL.SDL_WindowFlags.SDL_WINDOW_SHOWN);
-            if (window == IntPtr.Zero) {
-                LogSDLError(stdout, "CreateWindow");
-                SDL_image.IMG_Quit();
-                SDL.SDL_Quit();
-                return 1;
-            }
-
-            // Create renderer
-            IntPtr renderer = SDL.SDL_CreateRenderer(window, -1,
-                    SDL.SDL_RendererFlags.SDL_RENDERER_ACCELERATED |
-                    SDL.SDL_RendererFlags.SDL_RENDERER_PRESENTVSYNC);
-            if (renderer == IntPtr.Zero) {
-                LogSDLError(stdout, "CreateRenderer");
-                SDL.SDL_DestroyWindow(window);
-                SDL_image.IMG_Quit();
-                SDL.SDL_Quit();
-                return 1;
-            }
+            Window window = new Window("skaktego", 100, 100, SCREEN_WIDTH, SCREEN_HEIGHT);
+            Renderer renderer = new Renderer(window);
 
             // Load three images
-            IntPtr background = LoadTexture("background.png", renderer);
-            IntPtr image = LoadTexture("image.png", renderer);
-            IntPtr pieceSprites = LoadTexture("pieces.png", renderer);
-            if (background == IntPtr.Zero ||
-                image == IntPtr.Zero ||
-                pieceSprites == IntPtr.Zero) {
-                if (background != IntPtr.Zero) {
-                    SDL.SDL_DestroyTexture(background);
-                }
-                if (image != IntPtr.Zero) {
-                    SDL.SDL_DestroyTexture(image);
-                }
-                if (pieceSprites != IntPtr.Zero) {
-                    SDL.SDL_DestroyTexture(pieceSprites);
-                }
-                if (renderer != IntPtr.Zero) {
-                    SDL.SDL_DestroyRenderer(renderer);
-                }
-                if (window != IntPtr.Zero) {
-                    SDL.SDL_DestroyWindow(window);
-                }
-                SDL_image.IMG_Quit();
-                SDL.SDL_Quit();
-                return 1;
-            }
+            Texture background   = new Texture(renderer, "background.png");
+            Texture image        = new Texture(renderer, "image.png");
+            Texture pieceSprites = new Texture(renderer, "pieces.png");
 
             // Get the clips of the piece sprites.
-            SDL.SDL_Rect[,] pieceClips = GetPieceClips(pieceSprites);
+            Rect[,] pieceClips = GetPieceClips(pieceSprites);
 
             // The image offset
             int xOffset = 0, yOffset = 0;
@@ -134,8 +76,7 @@ namespace skaktego {
                     }
                 }
 
-
-                SDL.SDL_RenderClear(renderer);
+                renderer.Clear();
 
                 {
                     // Determine how many tiles we'll need to fill the screen
@@ -146,7 +87,7 @@ namespace skaktego {
                     for (int i = 0; i < xTiles * yTiles; ++i) {
                         int xT = i % xTiles;
                         int yT = i / xTiles;
-                        RenderTexture(background, renderer,
+                        renderer.RenderTexture(background,
                                 xT * TILE_SIZE, yT * TILE_SIZE, TILE_SIZE,
                                 TILE_SIZE);
                     }
@@ -155,132 +96,45 @@ namespace skaktego {
                 // Draw a moving chess piece.
                 {
                     DateTime now = DateTime.Now;
-                    SDL.SDL_Rect pieceDst;
-                    pieceDst.x = ((int)(now.Subtract(start).TotalMilliseconds * 0.3) % (SCREEN_WIDTH + 100)) - 100;
-                    pieceDst.y = (int)(now.Subtract(start).TotalMilliseconds * 0.25) % (SCREEN_HEIGHT + 100) - 100;
-                    pieceDst.w = 200;
-                    pieceDst.h = 200;
+                    Rect pieceDst = new Rect(0, 0, 200, 200);
+                    pieceDst.XPos = ((int)(now.Subtract(start).TotalMilliseconds * 0.3) % (SCREEN_WIDTH + 100)) - 100;
+                    pieceDst.YPos = (int)(now.Subtract(start).TotalMilliseconds * 0.25) % (SCREEN_HEIGHT + 100) - 100;
                     int t = ((int)now.Second / 2) % Piece.PIECE_TYPE_COUNT;
                     int c = (int) currentColor;
-                    RenderTexture(pieceSprites, renderer, ref pieceDst, ref pieceClips[t, c]);
+                    renderer.RenderTexture(pieceSprites, pieceDst, pieceClips[t, c]);
                 }
 
                 // Draw the foreground image
                 {
                     int iW, iH;
-                    SDL.SDL_QueryTexture(image, out _, out _, out iW, out iH);
+                    image.Query(out iW, out iH);
                     int x = SCREEN_WIDTH / 2 - iW / 2 + xOffset;
                     int y = SCREEN_HEIGHT / 2 - iH / 2 + yOffset;
-                    RenderTexture(image, renderer, x, y);
+                    renderer.RenderTexture(image, x, y);
                 }
 
-                SDL.SDL_RenderPresent(renderer);
+                renderer.Present();
             }
 
-            // Clean up
-            SDL.SDL_FreeSurface(background);
-            SDL.SDL_DestroyTexture(image);
-            SDL.SDL_DestroyRenderer(renderer);
-            SDL.SDL_DestroyWindow(window);
-            SDL_image.IMG_Quit();
-            SDL.SDL_Quit();
             return 0;
         }
 
-        static SDL.SDL_Rect[,] GetPieceClips(IntPtr tex) {
+        static Rect[,] GetPieceClips(Texture texture) {
             int tW, tH;
-            SDL.SDL_QueryTexture(tex, out _, out _, out tW, out tH);
-            SDL.SDL_Rect[,] clips = new SDL.SDL_Rect[Piece.PIECE_TYPE_COUNT, Piece.PIECE_COLOR_COUNT];
+            texture.Query(out tW, out tH);
+            Rect[,] clips = new Rect[Piece.PIECE_TYPE_COUNT, Piece.PIECE_COLOR_COUNT];
 
             int w = tW / Piece.PIECE_TYPE_COUNT;
             int h = tH / Piece.PIECE_COLOR_COUNT;
             for (int y = 0; y < Piece.PIECE_COLOR_COUNT; y++) {
                 for (int x = 0; x < Piece.PIECE_TYPE_COUNT; x++) {
-                    SDL.SDL_Rect clip;
-                    clip.x = x * w;
-                    clip.y = y * w;
-                    clip.w = w;
-                    clip.h = h;
-                    clips[x, y] = clip;
+                    clips[x, y] = new Rect(x * w, y * w, w, h);
                 }
             }
 
             return clips;
         }
 
-        /// <summary>
-        /// Log an SDL error with some error message to the output stream of our choice
-        /// <param name="writer">The TextWriter to write to</param>
-        /// <param name="msg">The message to write</param>
-        static void LogSDLError(TextWriter writer, string msg) {
-            using (writer) {
-                writer.WriteLine(msg + " error: " + SDL.SDL_GetError());
-            }
-        }
 
-        /// <summary>
-        /// Load an image into a texture on the rendering device
-        /// </summary>
-        /// <param name="file">The image file to load</param>
-        /// <param name="ren">The renderer to load the texture onto</param>
-        /// <returns>The loaded texture, or <c>IntPtr.Zero</c> if something went wrong</returns>
-        static IntPtr LoadTexture(string file, IntPtr ren) {
-            // Construct the full path
-            string path = Path.Combine(RESOURCE_PATH, file);
-
-            IntPtr texture = SDL_image.IMG_LoadTexture(ren, path);
-            if (texture == IntPtr.Zero) {
-                LogSDLError(stdout, "LoadTexture");
-            }
-            return texture;
-        }
-
-        /// <summary>
-        /// Draw an SDL_Texture to an SDL_Renderer at some destination rect
-        /// taking a clip of the texture if desired
-        /// </summary>
-        /// <param name="tex">The source texture we want to draw</param>
-        /// <param name="ren">The renderer we want to draw to</param>
-        /// <param name="dst">The destination rectangle to render the texture to</param>
-        /// <param name="clip">The sub-section of the texture to draw (clipping rect)
-        /// default of nullptr draws the entire texture</param>
-        static void RenderTexture(IntPtr tex, IntPtr ren, ref SDL.SDL_Rect dst,
-                ref SDL.SDL_Rect clip) {
-            SDL.SDL_RenderCopy(ren, tex, ref clip, ref dst);
-        }
-
-        /// <summary>
-        /// Draw an SDL_Texture to an SDL_Renderer at position x, y, with some desired
-        /// width and height
-        /// </summary>
-        /// <param name="tex">The source texture we want to draw</param>
-        /// <param name="ren">The renderer we want to draw to</param>
-        /// <param name="x">The x coordinate to draw to</param>
-        /// <param name="y">The y coordinate to draw to</param>
-        /// <param name="w">The width of the texture to draw</param>
-        /// <param name="h">The height of the texture to draw</param>
-        static void RenderTexture(IntPtr tex, IntPtr ren, int x, int y, int w, int h) {
-            //Setup the destination rectangle to be at the position we want
-            SDL.SDL_Rect dst;
-            dst.x = x;
-            dst.y = y;
-            dst.w = w;
-            dst.h = h;
-            SDL.SDL_RenderCopy(ren, tex, IntPtr.Zero, ref dst);
-        }
-
-        /// <summary>
-        /// Draw an SDL_Texture to an SDL_Renderer at position x, y, preserving
-        /// the texture's width and height
-        /// </summary>
-        /// <param name="tex">The source texture we want to draw</param>
-        /// <param name="ren">The renderer we want to draw to</param>
-        /// <param name="x">The x coordinate to draw to</param>
-        /// <param name="y">The y coordinate to draw to</param>
-        static void RenderTexture(IntPtr tex, IntPtr ren, int x, int y) {
-            int w, h;
-            SDL.SDL_QueryTexture(tex, out _, out _, out w, out h);
-            RenderTexture(tex, ren, x, y, w, h);
-        }
     }
 }
