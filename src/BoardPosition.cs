fa78f2245597b6c76f46654e714c208a980ccf3c
Cornelius Sevald-Krause <cornelius.sevald@gmail.com>
2020-02-14 15:06:07 +0100

    Fix BoardPosition

--- src/BoardPosition.cs
+++ src/BoardPosition.cs
@@ -2,26 +2,25 @@ using System;
 
 namespace skaktego {
 
-    public class BoardPosition : Tuple<int, int> {
+    public class BoardPosition {
 
-        public int Column {
-            get { return Item1; }
-        }
+        public int Column { get; set; }
 
-        public int Row {
-            get { return Item2; }
-        }
+        public int Row { get; set; }
 
         public static BoardPosition FromString(string pos) {
             // TODO: Throw error if px > 'i' or py > 9
-            char px = pos[0];
-            char py = pos[1];
+            char   px = pos[0];
+            string py = pos.Substring(1);
             int i = (int)(px - 'a');
-            int j = (int)(py - '1');
+            int j = int.Parse(py) - 1;
             return new BoardPosition(i, j);
         }
 
-        public BoardPosition(int column, int row) : base(column, row) { }
+        public BoardPosition(int column, int row) {
+            Column = column;
+            Row = row;
+        }
 
         public override string ToString() {
             // TODO: Throw error if px > 'i' or py > 9
