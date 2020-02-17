5f6b1cb7d609ef219f77ba540d879e34da193798
Cornelius Sevald-Krause <cornelius.sevald@gmail.com>
2020-02-17 11:15:57 +0100

    Make board positions comparable

--- src/BoardPosition.cs
+++ src/BoardPosition.cs
@@ -10,7 +10,7 @@ namespace skaktego {
 
         public static BoardPosition FromString(string pos) {
             // TODO: Throw error if px > 'i' or py > 9
-            char   px = pos[0];
+            char px = pos[0];
             string py = pos.Substring(1);
             int i = (int)(px - 'a');
             int j = int.Parse(py) - 1;
@@ -28,6 +28,22 @@ namespace skaktego {
             char py = (char)('1' + row);
             char[] charArr = { px, py };
             return new string(charArr);
+        }
+
+        public override bool Equals(Object obj) {
+            return obj is  BoardPosition && this == (BoardPosition)obj;
+        }
+
+        public override int GetHashCode() {
+            return column.GetHashCode() ^ row.GetHashCode();
+        }
+
+
+        public static bool operator ==(BoardPosition pos1, BoardPosition pos2) {
+            return pos1.column == pos2.column && pos1.row == pos2.row;
+        }
+        public static bool operator !=(BoardPosition pos1, BoardPosition pos2) {
+            return !(pos1 == pos2);
         }
     }
 
