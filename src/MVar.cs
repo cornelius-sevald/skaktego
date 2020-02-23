6b1c3bd393167eee8b2ca78d8cdef601aac109a5
Cornelius Sevald-Krause <cornelius.sevald@gmail.com>
2020-02-23 23:15:52 +0100

    Change MVar class

--- src/MVar.cs
+++ src/MVar.cs
@@ -1,63 +1,65 @@
 using System;
 using System.Threading;
 
-/// <summary>
-/// Synchronised variable.
-/// Inspired by Haskell's MVar: https://hackage.haskell.org/package/base-4.12.0.0/docs/Control-Concurrent-MVar.html
-/// 
-/// Taken from: https://www.pcreview.co.uk/threads/mvar-for-net.3816403/
-/// </summary>
-public class MVar<T>
-{
+namespace skaktego {
 
-    // Properties
-    private AutoResetEvent reader = new AutoResetEvent(false);
-    private AutoResetEvent writer = new AutoResetEvent(true);
+    /// <summary>
+    /// /// Synchronised variable.
+    /// Inspired by Haskell's MVar: https://hackage.haskell.org/package/base-4.12.0.0/docs/Control-Concurrent-MVar.html
+    /// 
+    /// Taken from: https://www.pcreview.co.uk/threads/mvar-for-net.3816403/
+    /// </summary>
+    public class MVar<T> {
 
-    private T varD;
-    public T var
-    {
-        get { return takeMVar(x => x); }
-        set { putMVar(value, x => { }); }
-    }
+        // Properties
+        private AutoResetEvent reader = new AutoResetEvent(false);
+        private AutoResetEvent writer = new AutoResetEvent(true);
 
-    // Constructors.
-    public MVar() { }
-    public MVar(T initial) : this() { var = initial; }
+        private T varD;
+        public T Var {
+            get { return TakeMVar(x => x); }
+            set { PutMVar(value, x => { }); }
+        }
 
-    // Set value or block.
-    public R putMVar<R>(T x, Func<T, R> f)
-    {
-        R res;
+        public bool HasValue { get; private set; }
 
-        writer.WaitOne();
-        varD = x;
-        res = f(varD);
-        reader.Set();
-        return res;
-    }
+        // Constructors.
+        public MVar() { HasValue = false; }
+        public MVar(T initial) : this() { Var = initial; }
 
-    // Set value or block.
-    public void putMVar(T v, Action<T> f)
-    {
-        putMVar(v, x => { f(x); return true; });
-    }
+        // Set value or block.
+        public R PutMVar<R>(T x, Func<T, R> f) {
+            R res;
 
-    // Get value or block.
-    public R takeMVar<R>(Func<T, R> f)
-    {
-        R v;
+            writer.WaitOne();
+            varD = x;
+            res = f(varD);
+            HasValue = true;
+            reader.Set();
+            return res;
+        }
 
-        reader.WaitOne();
-        v = f(varD);
-        writer.Set();
-        return v;
-    }
+        // Set value or block.
+        public void PutMVar(T v, Action<T> f) {
+            PutMVar(v, x => { f(x); return true; });
+        }
 
-    // Get value or block.
-    public T takeMVar(Action<T> f)
-    {
-        return takeMVar(x => { f(x); return x; });
+        // Get value or block.
+        public R TakeMVar<R>(Func<T, R> f) {
+            R v;
+
+            reader.WaitOne();
+            v = f(varD);
+            HasValue = false;
+            writer.Set();
+            return v;
+        }
+
+        // Get value or block.
+        public T TakeMVar(Action<T> f) {
+            return TakeMVar(x => { f(x); return x; });
+        }
+
     }
 
 }
